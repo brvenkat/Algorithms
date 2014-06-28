bc256666de6f802a9fe308063cad8a73b899c377
unknown <vbalachandran@L-AUN-00559153.corp.ebay.com>
2014-06-27 20:09:09 -0500

    Excercise

---
+++ Fibonacci.java
@@ -0,0 +1,52 @@
+package NonDataStructure;
+
+/**
+ * Function to calculate the first 10 fibonacci numbers.
+ * Prints the output on the screen/terminal using StringBuffer
+ * @author vbalachandran
+ *
+ */
+public class Fibonacci {
+	private static final int N = 9;
+	public static boolean[] printedArray = new boolean[10];
+	private static StringBuffer sb = new StringBuffer("0,1");
+	public static void main(String args[])
+	{
+		System.out.println("========Code to print the first 10 fibonacci numbers===============");
+		try
+		{
+			printedArray[0] = printedArray[1] =true;
+		    Fibonacci fb = new Fibonacci();
+		    System.out.println("The first 10 fibonacci numbers are ");
+		    fb.computeFibonacci(N);
+		    System.out.println(sb.toString());
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Helper function that recursively computes the fibonacci 
+	 * It uses a boolean array to see if we have already computed the fibonacci for a number
+	 * @param n
+	 * @return
+	 */
+	public int computeFibonacci(int n)
+	{
+       if(n == 0 || n==1)
+          return n;
+       
+       int number = computeFibonacci(n-1)+computeFibonacci(n-2);
+       
+	   if(!printedArray[n])
+	   {
+           sb.append(","+number);
+           printedArray[n] = true;
+	   }
+	   
+       return number;
+	}
+
+}
---
+++ atoi.java
@@ -0,0 +1,74 @@
+package NonDataStructure;
+
+/**
+ * Code to Convert ASCII to an Integer
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.Math;
+
+public class atoi {
+	
+	/**
+	 * The main function keeps reading an input until the user hits exit
+	 * It calls a helper function defined in atoi class that converts the given string into Integer
+	 * If the given string is not in a proper format, it displays an appropriate message
+	 * @param args
+	 */
+	public static void main(String args[])
+	{
+		String input ="";
+		while(true)
+		{
+			try 
+			{
+				System.out.println("Enter a character in ASCII. Type exit to quit");
+			    BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+			    input = bufferRead.readLine();
+				if(input.equalsIgnoreCase("exit"))
+					break;
+		        atoi at = new atoi();
+			    at.convertASCII(input);
+			} 
+			catch(NumberFormatException ne)
+			{
+				System.out.println("Not a valid number ");
+			}
+			catch(IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Terminating ...");
+	}
+	
+	/**
+	 * Helper function to convert String into an Integer. 
+	 * Throws a Numberformat Exception if any character in the input string cannot be converted to a number
+	 * @param asciiString
+	 */
+	private void convertASCII(String asciiString)
+	{
+		int startindex = 0;
+		int number=0;
+		boolean isNegative = false;
+		if(asciiString.charAt(0) == '-')
+		{
+			isNegative=true;
+		    startindex = 1;
+		}
+		for(int i=startindex;i<asciiString.length();i++)
+		{
+			int charValue = asciiString.charAt(i) - '0';
+			if(charValue < 0 || charValue > 9)
+				throw new NumberFormatException("This is not an Integer ");
+			number = number*10 + charValue;
+		}
+		if(isNegative)
+			number = number * -1;
+		
+		System.out.println("The Numeris value of String is "+number);
+	}
+
+}
